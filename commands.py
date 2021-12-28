d162a4d4fc0548771bbf34e7abb3b3491b41793f
Cibelle Priscila <cibellepriscila12@gmail.com>
2021-12-28 10:07:18 -0300

    segundo commit

--- commands.py
+++ commands.py
@@ -7,19 +7,20 @@ def start(update, context):
     message = (
         "Eu sou um bot que irá disponibilizar imagens de "
         "gatinhos para tornar seu dia mais feliz. \n\n"
-        "Comandos: \n"
-        "/meow - fasfds asdfsf  asfsadf"
+        "Comandos: \n\n"
+       
+        "/start - inicia o bot \n"
+        "/help - lista os comandos \n"
+        "/meow - envia uma foto de gatinho \n"
     )
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 def helpp(update, context):
     messagem = (
         "Comandos para usar no bot: \n"
-        "/start - inicia o bot"
-        "/help - lista os comandos"
-        "/meow - envia uma foto de gatinho"
-        "/infinite_image - envia continuamente fotos de gatinho"
-        "/stop_loop - Para o envio de fotos de gatinho"
+        "/start - inicia o bot \n"
+        "/help - lista os comandos \n"
+        "/meow - envia uma foto de gatinho \n"
     )
     context.bot.send_message(chat_id=update.effective_chat.id, text=messagem)
 
@@ -29,10 +30,3 @@ def send_image(update, context):
     url_photo = contents.json()[0]["url"]
     chat_id = update.message.chat_id
     context.bot.send_photo(chat_id=chat_id, photo=url_photo)
-
-def infinite_image(update, context):
-    while True:
-        send_image(update, context)
-
-def stop_loop(update, context):
-    Updater.stop()
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from telegram.ext import Updater, CommandHandler
 from config import settings
-from commands import send_image
-from commands import start
-from commands import infinite_image
-from commands import stop_loop
-from commands import helpp
+from commands import send_image, helpp, start
+
+
 def main():
     updater = Updater(token=settings.TOKEN, use_context=True)
     updater.dispatcher.add_handler(CommandHandler("start", start))
     updater.dispatcher.add_handler(CommandHandler("meow", send_image))
-    updater.dispatcher.add_handler(CommandHandler("infinite_image", infinite_image))
-    updater.dispatcher.add_handler(CommandHandler("stop_loop", stop_loop))
     updater.dispatcher.add_handler(CommandHandler("help", helpp))
+    updater.start_polling()
     updater.idle()
-
+    
 
 if __name__ == "__main__":
     main()
